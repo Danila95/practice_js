907f972f8ba0e0a79d52c232055e003f8381344d
ddb4 <ddb4@tpu.ru>
2023-03-16 17:33:27 +0700

    complete lesson 10 Redux-thunk

---
+++ Полный курс по JavaScript + React - с нуля до результата/React/Redux/10 Redux-thunk/hero_admin_panel_template/src/actions/index.js
@@ -0,0 +1,66 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching);
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const heroesFetching = () => {
+    return {
+        type: 'HEROES_FETCHING'
+    }
+}
+
+export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+
+export const addHeroe = (data) => {
+    return {
+        type: 'ADD_HEROE',
+        payload: data
+    }
+}
+
+export const removeHeroe = (id) => {
+    return {
+        type: 'REMOVE_HEROE',
+        payload: id
+    }
+}
+
+export const filtersFetching = () => {
+    return {
+        type: 'FILTERS_FETCHEDING'
+    }
+}
+
+export const filtersFetched = (data) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: data
+    }
+}
+
+export const activeFilterChanged = (filter) => {
+    return {
+        type: 'ACTIVE_FILTER_CHANGED',
+        payload: filter
+    }
+}
+
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
