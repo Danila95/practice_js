cc99a2fa9c5475b38b8f592e5d2e8879400b6d80
ddb4 <ddb4@tpu.ru>
2023-03-17 15:48:52 +0700

    complete lesson 12 Redux Toolkit createAction()

---
+++ Полный курс по JavaScript + React - с нуля до результата/React/Redux/12 Redux Toolkit createAction()/hero_admin_panel_template/src/actions/index.js
@@ -0,0 +1,75 @@
+import { createAction } from '@reduxjs/toolkit'
+
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching);
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const heroesFetching = (request) => (dispatch) => {
+    dispatch('HEROES_FETCHING');
+    request("http://localhost:3001/filters")
+        .then(data => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()))
+}
+
+
+// export const heroesFetched = (heroes) => {
+//     return {
+//         type: 'HEROES_FETCHED',
+//         payload: heroes
+//     }
+// }
+
+export const heroesFetched = createAction('HEROES_FETCHED')
+
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+
+export const addHeroe = (data) => {
+    return {
+        type: 'ADD_HEROE',
+        payload: data
+    }
+}
+
+export const removeHeroe = (id) => {
+    return {
+        type: 'REMOVE_HEROE',
+        payload: id
+    }
+}
+
+// export const filtersFetching = () => {
+//     return {
+//         type: 'FILTERS_FETCHEDING'
+//     }
+// }
+
+export const filtersFetching = createAction('FILTERS_FETCHEDING')
+
+export const filtersFetched = (data) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: data
+    }
+}
+
+export const activeFilterChanged = (filter) => {
+    return {
+        type: 'ACTIVE_FILTER_CHANGED',
+        payload: filter
+    }
+}
+
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
