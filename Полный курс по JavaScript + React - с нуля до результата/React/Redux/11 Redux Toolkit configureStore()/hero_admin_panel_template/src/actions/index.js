5f66e8c541d91acfe77ba2776335c3a69eb918bc
ddb4 <ddb4@tpu.ru>
2023-03-17 15:15:08 +0700

    complete lesson 11 Redux Toolkit configureStore()

---
+++ Полный курс по JavaScript + React - с нуля до результата/React/Redux/11 Redux Toolkit configureStore()/hero_admin_panel_template/src/actions/index.js
@@ -0,0 +1,67 @@
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching);
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
+
+export const heroesFetching = (request) => (dispatch) => {
+    dispatch('HEROES_FETCHING');
+    request("http://localhost:3001/filters")
+        .then(data => dispatch(filtersFetched(data)))
+        .catch(() => dispatch(filtersFetchingError()))
+}
+
+export const heroesFetched = (heroes) => {
+    return {
+        type: 'HEROES_FETCHED',
+        payload: heroes
+    }
+}
+
+export const heroesFetchingError = () => {
+    return {
+        type: 'HEROES_FETCHING_ERROR'
+    }
+}
+
+
+export const addHeroe = (data) => {
+    return {
+        type: 'ADD_HEROE',
+        payload: data
+    }
+}
+
+export const removeHeroe = (id) => {
+    return {
+        type: 'REMOVE_HEROE',
+        payload: id
+    }
+}
+
+export const filtersFetching = () => {
+    return {
+        type: 'FILTERS_FETCHEDING'
+    }
+}
+
+export const filtersFetched = (data) => {
+    return {
+        type: 'FILTERS_FETCHED',
+        payload: data
+    }
+}
+
+export const activeFilterChanged = (filter) => {
+    return {
+        type: 'ACTIVE_FILTER_CHANGED',
+        payload: filter
+    }
+}
+
+export const filtersFetchingError = () => {
+    return {
+        type: 'FILTERS_FETCHING_ERROR'
+    }
+}
---
+++ Полный курс по JavaScript + React - с нуля до результата/React/Redux/11 Redux Toolkit configureStore()/hero_admin_panel_template/src/components/heroesAddForm/HeroesAddForm.js
@@ -0,0 +1,96 @@
+import { v4 as uuidv4 } from 'uuid';
+import {useHttp} from '../../hooks/http.hook'
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { addHeroe } from '../../actions'
+
+// Задача для этого компонента:
+// Реализовать создание нового героя с введенными данными. Он должен попадать
+// в общее состояние и отображаться в списке + фильтроваться
+
+const HeroesAddForm = () => {
+    const dispatch = useDispatch()
+    const [name, setName] = useState('')
+    const [description, setDescription] = useState('')
+    const [element, setElement] = useState('')
+    const [elements, setElements] = useState([])
+    const {request} = useHttp();
+
+    useEffect(() => {
+        request("http://localhost:3001/filters")
+            .then(data => setElements(data.filter(item => item.name !== 'all')))
+
+        // eslint-disable-next-line
+    }, []);
+
+    const handlerAddHeroe = (e) => {
+        e.preventDefault()
+        const newHero = {
+            id: uuidv4(),
+            name,
+            description,
+            element
+        }
+        request("http://localhost:3001/heroes", 'POST', JSON.stringify(newHero))
+            .then(res => console.log(res, 'Отправка успешна'))
+            .then(dispatch(addHeroe(newHero)))
+            .catch(err => console.log(err))
+
+        setName('')
+        setDescription('')
+        setElement('')
+    }
+
+    return (
+        <form className="border p-4 shadow-lg rounded">
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label fs-4">Имя нового героя</label>
+                <input 
+                    required
+                    type="text" 
+                    name="name" 
+                    className="form-control" 
+                    id="name" 
+                    placeholder="Как меня зовут?"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="text" className="form-label fs-4">Описание</label>
+                <textarea
+                    required
+                    name="text" 
+                    className="form-control" 
+                    id="text" 
+                    placeholder="Что я умею?"
+                    style={{"height": '130px'}}
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+            </div>
+
+            <div className="mb-3">
+                <label htmlFor="element" className="form-label">Выбрать элемент героя</label>
+                <select 
+                    required
+                    className="form-select" 
+                    id="element" 
+                    name="element"
+                    value={element}
+                    onChange={(e) => setElement(e.target.value)}
+                >
+                    <option >Я владею элементом...</option>
+                    {elements.map(item =>
+                        <option key={item.name} value={item.name}>{item.label}</option>
+                    )}
+                </select>
+            </div>
+
+            <button type="submit" className="btn btn-primary" onClick={(e) => handlerAddHeroe(e)}>Создать</button>
+        </form>
+    )
+}
+
+export default HeroesAddForm;
