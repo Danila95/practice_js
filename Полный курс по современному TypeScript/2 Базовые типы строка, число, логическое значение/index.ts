b1a13a963226569b33ea546cfd9d26f7e66561e2
ddb4 <ddb4@tpu.ru>
2023-03-23 17:02:35 +0700

    complete lesson ts 2 Базовые типы строка, число, логическое значение

---
+++ Полный курс по современному TypeScript/2 Базовые типы строка, число, логическое значение/index.ts
@@ -0,0 +1,12 @@
+// let userName: string = 'Dan'
+
+// userName = ''
+
+const isBirthday: boolean = true
+let age: number = 40
+const userName: string = 'John'
+
+
+if (isBirthday) {
+	console.log(`Congrats ${userName.toLocaleUpperCase()}, age: ${age + 1}`)
+}
